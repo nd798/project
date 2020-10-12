e5080b24ea69c71f3a654ce9e3a6d33ecaa5c534
nd798 <72391163+nd798@users.noreply.github.com>
2020-10-12 09:42:48 +0300

    Initial commit

---
+++ src/test/java/com/dmitrii/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmitrii.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
